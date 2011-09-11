eb8187c207b5f6e010d3a000c6407decdd37d067
derek@battams.ca <derek@battams.ca@095d6378-61bf-9fdc-c12e-89473da28212>
2011-09-11 03:33:08 +0000

    Refactor package name (again)

--- java/trunk/src/com/google/code/livepvrdataclients/ResponseFactory.java
+++ java/trunk/src/com/google/code/livepvrdataclients/ResponseFactory.java
@@ -13,7 +13,7 @@
  *       See the License for the specific language governing permissions and
  *       limitations under the License.
  */
-package com.google.code.livepvrdataclients.java;
+package com.google.code.livepvrdataclients;
 
 import org.json.JSONException;
 import org.json.JSONObject;
