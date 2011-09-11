00fcf46c5d531f65ed636c48f5cdad7e73151deb
derek <derek@battams.ca>
2011-09-11 03:31:14 +0000

    More solution refactoring

--- dotnet/trunk/LivePvrDataDotNet/Properties/AssemblyInfo.cs
+++ dotnet/trunk/LivePvrDataDotNet/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LivePvrDataDotNet")]
+[assembly: AssemblyTitle("LivePvrData")]
 [assembly: AssemblyDescription("A DotNet client for the Live PVR Data service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
