bb0b13ed085d75f7845b19d078c2dd9345ee2f94
derek <derek@battams.ca>
2011-09-11 02:35:54 +0000

    Rename the package

--- java/trunk/src/com/google/code/livepvrdataclients/java/Client.java
+++ java/trunk/src/com/google/code/livepvrdataclients/java/Client.java
@@ -1,5 +1,5 @@
 /*
- *      Copyright 2010 livepvrdata.com
+ *      Copyright 2010-2011 livepvrdata.com
  *       
  *       Licensed under the Apache License, Version 2.0 (the "License");
  *       you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  *       See the License for the specific language governing permissions and
  *       limitations under the License.
  */
-package com.google.code.livepvrdata4j;
+package com.google.code.livepvrdataclients.java;
 
 import java.io.IOException;
 import java.util.ArrayList;
